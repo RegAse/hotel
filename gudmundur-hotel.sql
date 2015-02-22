07016e1bbc30eb012fd897abc6e734ff757019e3
RegAse <brucejackie21@gmail.com>
2015-02-22 00:44:48 +0000

    Finished

--- gudmundur-hotel.sql
+++ gudmundur-hotel.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS gudmundur_hotel;
 
 -- Countries
-CREATE TABLE IF NOT EXISTS `countries` (
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`countries` (
   `alpha336612` char(2) NOT NULL,
   `alpha336613` char(3) NOT NULL,
   `alpha31662` char(13) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `countries` (
 );
 
 -- Guests
-CREATE TABLE IF NOT EXISTS `guests` (
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`guests` (
   `guestID` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(45) NOT NULL,
   `lastName` varchar(45) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `guests` (
 );
 
 -- Hotels
-CREATE TABLE IF NOT EXISTS `hotels` (
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`hotels` (
   `hotelID` int(11) NOT NULL AUTO_INCREMENT,
   `hotelName` varchar(55) NOT NULL,
   `streetAddress` varchar(125) NOT NULL,
@@ -32,10 +32,40 @@ CREATE TABLE IF NOT EXISTS `hotels` (
   FOREIGN KEY (countryID) REFERENCES countries(alpha336612)
 );
 
-CREATE TABLE IF NOT EXISTS `comments` (
+-- Comments
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`comments` (
   `commentID` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(350) NOT NULL,
   `guestID` int(11) NOT NULL,
   PRIMARY KEY (`commentID`),
   FOREIGN KEY (guestID) REFERENCES guests(guestID)
+);
+
+-- Bookings
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`bookings` (
+  `bookingNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `checkIn` date NOT NULL,
+  `checkOut` date NOT NULL,
+  `dateOfBooking` datetime NOT NULL,
+  `roomNumber` int(11) NOT NULL,
+  `guestID` int(11) NOT NULL,
+  PRIMARY KEY (`bookingNumber`),
+  FOREIGN KEY (guestID) REFERENCES guests(guestID)
+);
+
+-- Rooms
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`rooms` (
+  `roomNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `roomTypeID` int(11) NOT NULL,
+  `recommendedPrice` int(11) NOT NULL,
+  `hotelID` int(11) NOT NULL,
+  PRIMARY KEY (`roomNumber`),
+  FOREIGN KEY (hotelID) REFERENCES hotels(hotelID)
+);
+
+-- RoomType
+CREATE TABLE IF NOT EXISTS gudmundur_hotel.`roomtype` (
+  `roomTypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `roomType` varchar(120) NOT NULL,
+  PRIMARY KEY (`roomTypeID`)
 );
